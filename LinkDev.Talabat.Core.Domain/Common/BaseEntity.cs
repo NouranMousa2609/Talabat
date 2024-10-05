e8d10da65622a6f6f919eb6aeb527d6a9b7656c9
NouranMousa2609 <nouranmousa55@gmail.com>
2024-10-05 18:32:12 +0300

    Part 02 BaseEntity

---
+++ LinkDev.Talabat.Core.Domain/Common/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Common
+{
+	public abstract class BaseEntity<Tkey> where Tkey : IEquatable<Tkey>
+	{
+		public required Tkey Id  { get; set; }
+
+		public required string Createdby { get; set; }
+		public DateTime CreatedOn { get; set; } /*= DateTime.UtcNow;*/
+
+		public required string LastModifiedBy { get; set; }
+		public DateTime LastModifiedOn { get; set; } /*= DateTime.UtcNow;*/
+
+
+
+	}
+}

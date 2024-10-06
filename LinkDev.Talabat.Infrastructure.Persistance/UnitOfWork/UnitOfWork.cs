2f6df9b48abc8af21be87a512802e833993931f2
NouranMousa2609 <nouranmousa55@gmail.com>
2024-10-07 02:35:09 +0300

    Part 10 Unit Of Work [Final Implementation]

--- LinkDev.Talabat.Infrastructure.Persistance/UnitOfWork/UnitOfWork.cs
+++ LinkDev.Talabat.Infrastructure.Persistance/UnitOfWork/UnitOfWork.cs
@@ -1,54 +1,58 @@
-﻿using LinkDev.Talabat.Core.Domain.Contracts;
-using LinkDev.Talabat.Core.Domain.Entities.Products;
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using LinkDev.Talabat.Core.Domain.Contracts;
 using LinkDev.Talabat.Infrastructure.Persistence.Data;
 using LinkDev.Talabat.Infrastructure.Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace LinkDev.Talabat.Infrastructure.Persistence.UnitOfWork
 {
 	internal class UnitOfWork : IUnitOfWork
 	{
 		private readonly StoreContext _dbContext;
-		private readonly Lazy<IGenericRepository<Product,int>> _productRepository;
-		private readonly Lazy<IGenericRepository<ProductBrand,int>> _BrandsRepository;
-		private readonly Lazy<IGenericRepository<ProductCategory,int>> _CategoriesRepository;
+		///private readonly Lazy<IGenericRepository<Product,int>> _productRepository;
+		///private readonly Lazy<IGenericRepository<ProductBrand,int>> _BrandsRepository;
+		///private readonly Lazy<IGenericRepository<ProductCategory,int>> _CategoriesRepository;
 		
-
+		private readonly ConcurrentDictionary<string, object> _Repositories;
 
 		public UnitOfWork( StoreContext context)
         {
 			_dbContext = context;
-			_productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext));
-			_BrandsRepository = new Lazy<IGenericRepository<ProductBrand, int>>(() => new GenericRepository<ProductBrand, int>(_dbContext));
-			_CategoriesRepository = new Lazy<IGenericRepository<ProductCategory, int>>(() => new GenericRepository<ProductCategory, int>(_dbContext));
-			//_productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext), LazyThreadSafetyMode.ExecutionAndPublication);
-			//_productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext), true);
-			//ProductRepository = new GenericRepository<Product,int>(_dbContext);
-			//BrandsRepository=new GenericRepository<ProductBrand,int>(_dbContext);
-			//CategoriesRepository=new GenericRepository<ProductCategory,int>(_dbContext);
+			_Repositories = new ();
+
+
+			///_productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext));
+			///_BrandsRepository = new Lazy<IGenericRepository<ProductBrand, int>>(() => new GenericRepository<ProductBrand, int>(_dbContext));
+			///_CategoriesRepository = new Lazy<IGenericRepository<ProductCategory, int>>(() => new GenericRepository<ProductCategory, int>(_dbContext));
+			///_productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext), LazyThreadSafetyMode.ExecutionAndPublication);
+			///_productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext), true);
+			///ProductRepository = new GenericRepository<Product,int>(_dbContext);
+			///BrandsRepository=new GenericRepository<ProductBrand,int>(_dbContext);
+			///CategoriesRepository=new GenericRepository<ProductCategory,int>(_dbContext);
 		}
-        public IGenericRepository<Product, int> ProductRepositore =>  _productRepository.Value;
-			
-		
 
-		public IGenericRepository<ProductBrand, int> BrandsRepositore=> _BrandsRepository.Value;
-		
 
-		public IGenericRepository<ProductCategory, int> CategoriesRepositore => _CategoriesRepository.Value;
-		
+		/// public IGenericRepository<Product, int> ProductRepositore =>  _productRepository.Value;
+		/// public IGenericRepository<ProductBrand, int> BrandsRepositore=> _BrandsRepository.Value;
+		///public IGenericRepository<ProductCategory, int> CategoriesRepositore => _CategoriesRepository.Value;
 
-		public Task<int> CompleteAsync()
+		public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+			where TEntity : BaseEntity<TKey>
+			where TKey : IEquatable<TKey>
 		{
-			throw new NotImplementedException();
-		}
+			///var	typeName =typeof(TEntity).Name; //Product
+			///if (_Repositories.ContainsKey(typeName)) return (IGenericRepository<TEntity, TKey>) _Repositories[typeName];
+			///
+			///var Repository = new GenericRepository<TEntity, TKey>(_dbContext);
+			///_Repositories.Add(typeName, Repository);
+			///return Repository;
+			///
+			return(IGenericRepository<TEntity, TKey>)_Repositories.GetOrAdd(typeof(TEntity).Name,(Key)=> new GenericRepository<TEntity, TKey>(_dbContext));
 
-		public ValueTask DisposeAsync()
-		{
-			throw new NotImplementedException();
 		}
+		public async Task<int> CompleteAsync()=> await _dbContext.SaveChangesAsync();
+		public async ValueTask DisposeAsync()=>await _dbContext.DisposeAsync();
+
+		
 	}
 }

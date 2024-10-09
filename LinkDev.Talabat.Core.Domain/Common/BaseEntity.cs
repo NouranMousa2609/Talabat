33f1467c7d9852dabdf2cbfbab6782722b13aace
NouranMousa2609 <nouranmousa55@gmail.com>
2024-10-09 14:29:42 +0300

    Part 01 BaseEntity and BaseAuditableEntity

--- LinkDev.Talabat.Core.Domain/Common/BaseEntity.cs
+++ LinkDev.Talabat.Core.Domain/Common/BaseEntity.cs
@@ -10,13 +10,7 @@ namespace LinkDev.Talabat.Core.Domain.Common
 	{
 		public required Tkey Id  { get; set; }
 
-		public required string CreatedBy { get; set; }
-		public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-
-		public required string LastModifiedBy { get; set; }
-		public DateTime LastModifiedOn { get; set; } = DateTime.UtcNow;
-
-
+		
 
 	}
 }

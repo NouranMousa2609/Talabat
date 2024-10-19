b68d3e715e6abfe8f6503be4058888a164cf54db
NouranMousa2609 <nouranmousa55@gmail.com>
2024-10-19 16:16:18 +0300

    Part01-Project SetUp

--- LinkDev.Talabat.Dashboard/Program.cs
+++ LinkDev.Talabat.Dashboard/Program.cs
@@ -1,3 +1,6 @@
+using LinkDev.Talabat.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace LinkDev.Talabat.Dashboard
 {
 	public class Program
@@ -8,6 +11,10 @@ namespace LinkDev.Talabat.Dashboard
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
+			builder.Services.AddDbContext<StoreContext>(options =>
+			{
+				options.UseSqlServer(builder.Configuration.GetConnectionString("StoreContext"));
+			});
 
 			var app = builder.Build();
 
